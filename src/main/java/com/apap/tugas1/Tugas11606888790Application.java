d667d602206aa9f37e3816ec3ce332a0364ab3e7
Fany Azka Hidayatulloh <azkahebat12@gmail.com>
2018-10-18 23:03:05 +0700

    initial commit

---
+++ src/main/java/com/apap/tugas1/Tugas11606888790Application.java
@@ -0,0 +1,12 @@
+package com.apap.tugas1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tugas11606888790Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tugas11606888790Application.class, args);
+	}
+}
